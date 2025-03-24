2ceb9ce5c629aec12c670e1de22d092bfd0469a6
Antonio Gaido <nadsonjunior@live.com>
2025-03-24 00:20:47 -0300

    Heroku config

--- src/main/java/org/desafio/WebConfig/WebConfig.java
+++ src/main/java/org/desafio/WebConfig/WebConfig.java
@@ -7,12 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("https://cadastropessoabackend-6a32f0706386.herokuapp.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
