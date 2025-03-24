c320e78730faa0a60102c891fdad328b2a7f2a0d
Antonio Gaido <nadsonjunior@live.com>
2025-03-24 01:13:39 -0300

    Heroku Config

--- src/main/java/org/desafio/WebConfig/WebConfig.java
+++ src/main/java/org/desafio/WebConfig/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOriginPatterns("https://cadastropessoabackend-6a32f0706386.herokuapp.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
         };
